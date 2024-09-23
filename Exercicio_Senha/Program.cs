da4abbcd19f6e1c3df8222920cc8ac99d3d64f76
carnielg <guicarniell22@gmail.com>
2024-09-23 02:23:15 -0300

    Initial commit

---
+++ Exercicio_Senha/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Senha
+{
+    class Program
+    {
+        static void Main()
+        {
+            Senha senha = new Senha(Console.ReadLine());
+            Console.WriteLine(senha.VerificarSeguranca());  // Saída: Senha forte.
+            Console.ReadKey();
+        }
+    }
+}
